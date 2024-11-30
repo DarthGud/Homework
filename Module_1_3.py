cd14be859fa7cde994f6546314fb54db3a9f8494
Andrey <Andrey.071161@gmail.com>
2024-11-30 12:48:31 +0300

    30/11

---
+++ Module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Андрей'
+print ('Имя: ', name)
+age = 23
+print ('Возраст: ' , age)
+new_age = 24
+print ('New age: ' , new_age)
+is_student = (5 > 3)
+print ('Is_student: ' , is_student)
