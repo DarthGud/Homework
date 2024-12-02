d2ff5de6b5e8be110c975cc8a72c0ff531c168f2
DarthGud <Andrey.071161@gmail.com>
2024-12-02 23:16:37 +0300

    Update Module_1_3.py
    02/12

--- Module_1_3.py
+++ Module_1_3.py
@@ -4,7 +4,7 @@ name = 'Андрей'
 print ('Имя: ', name)
 age = 23
 print ('Возраст: ' , age)
-new_age = 24
-print ('New age: ' , new_age)
+age = 24
+print ('New age: ' , age)
 is_student = (5 > 3)
 print ('Is_student: ' , is_student)
