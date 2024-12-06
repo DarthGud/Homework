e01522b573c6c722678b1b36da45b0e6725cb583
DarthGud <Andrey.071161@gmail.com>
2024-12-06 14:40:31 +0300

    Add files via upload

---
+++ Module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = 1, 'Cucumber', True
+print (immutable_var)
+immutable_var = immutable_var + (3, "Peach", False)
+print (immutable_var)
+mutable_list = ['Russia', 'USA', 'Italia']
+print (mutable_list)
+mutable_list .append('Japan')
+print (mutable_list)
+mutable_list .extend('China')
+print (mutable_list)
